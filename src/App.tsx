079b6024fdbf085004f1a284eece3c72a05ca930
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-05-28 21:37:27 -0400

    File Structure Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './components/Home';
-import Messages from './components/Messages';
-import Schedule from './components/Schedule';
-import Feed from './components/Feed';
-import Activity from './components/Activity';
-import Profile from './components/Profile';
+import Home from './_root/pages/Home';
+import Messages from './_root/pages/Messages';
+import Schedule from './_root/pages/Schedule';
+import Feed from './_root/pages/Feed';
+import Activity from './_root/pages/Activity';
+import Profile from './_root/pages/Profile';
+import Login from './_auth/forms/Login';
+import SignUp from './_auth/forms/SignUp';
+import RootLayout from './_root/RootLayout';
+import AuthLayout from './_auth/AuthLayout';
+import { Root } from 'postcss';
 
 function App() {
   return (
     <>
       <Router>
 		<Routes>
-			<Route path="/" element={<Home />}/>
-			<Route path="/messages" element={<Messages />}/>
-			<Route path="/schedule" element={<Schedule />} />
-			<Route path="/feed" element={<Feed />}/>
-			<Route path="/activity" element={<Activity />} />
-			<Route path="/profile" element={<Profile />}/>
+			{/* public routes */}
+			<Route element={<AuthLayout />}>
+				<Route path="/login" element={<Login/>}/>
+				<Route path="/sign-up" element={<SignUp />}/>
+			</Route>
+
+			{/* private routes */}
+			<Route element={<RootLayout />}>
+				<Route index path="/" element={<Home />}/>
+				<Route path="/messages" element={<Messages />}/>
+				<Route path="/schedule" element={<Schedule />} />
+				<Route path="/feed" element={<Feed />}/>
+				<Route path="/activity" element={<Activity />} />
+				<Route path="/profile" element={<Profile />}/>
+			</Route>
 		</Routes>
 	  </Router>
     </>
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,9 @@
+import Home from "./pages/Home"
+
+const RootLayout = () => {
+  return (
+	<Home />
+  )
+}
+
+export default RootLayout
