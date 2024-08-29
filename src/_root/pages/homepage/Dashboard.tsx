ac44f218991fe2c3bc4653ffe342fe49fcc614ed
Yiyan Huang <yiyanhuang0523@gmail.com>
2024-08-29 15:58:28 -0400

    Started Search BAr

--- src/_root/pages/homepage/Dashboard.tsx
+++ src/_root/pages/homepage/Dashboard.tsx
@@ -1,7 +1,14 @@
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Dashboard = () => {
   return (
-	<div>Dashboard</div>
+	<div className="flex flex-grow w-full p-10 justify-center">
+		<input type="text" placeholder="Search..." className="search-bar placeholder-l-blue placeholder:font-montserrat bg-white border-2 border-d-blue rounded text-d-blue pl-2"></input>
+		<button className="add-button bg-transparent flex items-center justify-center text-d-blue">
+			<FontAwesomeIcon icon={faCirclePlus}/>
+		</button>
+	</div>
   )
 }
 
--- src/_root/pages/homepage/Home.tsx
+++ src/_root/pages/homepage/Home.tsx
@@ -4,7 +4,7 @@ import './styling/Home.css'
 
 const Home = () => {
   return (
-	<div className="app flex flex-row">
+	<div className="app flex flex-row w-screen">
 		<NavBar />
 		<Dashboard />
 	</div>
